ad3ea1a5cba506f66c4cd405665f4164a8e35051
Anthony Luaders <anthony.luaders@anthonys-mbp.attlocal.net>
2020-08-08 15:27:00 -0400

    Added donate button to header

--- site/components/Header/index.js
+++ site/components/Header/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+import useStyles from './styles';
+
 const Header = props => {
+    const classes = useStyles();
+
     return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <Typography variant="h6">
-                    Extra Life - Slalom Atlanta
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Extra Life - Slalom Atlanta
+                    </Typography>
+                    <Button color="secondary" variant="contained" href="https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=412259#donate" target="_blank">Donate</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 };
 
