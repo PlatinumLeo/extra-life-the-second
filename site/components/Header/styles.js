87211ba2ded6af5566060cbb467aef513d05c524
aluaders <TonyLuaders@gmail.com>
2020-08-08 16:03:04 -0400

    Added donate button to header (#6)
    Co-authored-by: Anthony Luaders <anthony.luaders@anthonys-mbp.attlocal.net>

---
+++ site/components/Header/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+export default useStyles;
