5f160f56e6e5f75776f8cc79311a2bce45548c0c
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-08-10 17:41:39 -0400

    feat: grid and components

--- site/pages/LandingPage/index.js
+++ site/pages/LandingPage/index.js
@@ -1,48 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import {
-    Container,
+    Grid,
     Typography
 } from '@material-ui/core';
-import Skeleton from '@material-ui/lab/Skeleton'
 
 import Countdown from '../../components/Countdown';
-import ExtraLifeService from '../../services/ExtraLifeService';
-import useStyles from './styles';
+import Donation from '../../components/Donation';
 
 const LandingPage = props => {
     const dayOfPlay = new Date("Nov 11, 2020 11:00:00").getTime();
-    const [team, setTeam] = useState(null);
-
-    useEffect(() => {
-        initialize();
-    }, []);
-
-    const initialize = () => {
-        ExtraLifeService.getTeam()
-            .then(response => setTeam(response.data))
-            .catch(err => console.log(err));
-    };
-
-    const goalComponent = () => {
-        if (!team) {
-            return (<Skeleton variant="text" />);
-        }
-
-        return (
-            <Typography variant="h2">
-                Goal: ${team.sumDonations}/${team.fundraisingGoal}
-            </Typography>
-        );
-    };
 
     return (
-        <Container>
-            <Typography variant="h1">Extra Life Slalom Atlanta</Typography>
-            {goalComponent()}
-            <Typography variant="h2">Day of Play: November 7th 11:00 AM ET</Typography>
-            <Countdown dayOfPlay={dayOfPlay} />
-        </Container>
+        <Grid container spacing={3}>
+            <Grid item xs={12} lg={6}>
+                <Donation />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+                <Typography variant="h2">Day of Play: November 7th 11:00 AM ET</Typography>
+                <Countdown dayOfPlay={dayOfPlay} />
+            </Grid>
+        </Grid>
     );
 };
 
