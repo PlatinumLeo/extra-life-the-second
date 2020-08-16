812d0bf108027201a9bd4e7acc16079ce1ff8849
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-08-15 23:32:37 -0400

    feat: add simple caching (#18)

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,28 @@ import express from 'express';
 
 import teams from './teams';
 
+let cache = new Map();
+let cacheExpiration = new Map();
 const router = express.Router();
+const EXPIRATION_TIMEOUT = 5 * 60 * 1000; // 5 Minute Cache Timer
 
-router.use('/teams', teams);
+let middleware = (req, res, next) => {
+    const key = req.url;
+    let now = new Date().getTime();
+    let itemExpired = !cacheExpiration.has(key) || (now - cacheExpiration.get(key)) >= EXPIRATION_TIMEOUT;
+    if (cache.has(key) && ! itemExpired) {
+        res.send(cache.get(key));
+    } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            cache.set(key, body);
+            cacheExpiration.set(key, now);
+            res.sendResponse(body);
+        }
+        next();
+    }
+};
+
+router.use('/teams', [middleware, teams]);
 
 export default router;
