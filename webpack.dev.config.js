164dca334010bc26e6485e3f3f3df77d2fab5a5e
aluaders <TonyLuaders@gmail.com>
2020-08-15 21:51:15 -0400

    Added web banner images (#16)
    Co-authored-by: Anthony Luaders <anthony.luaders@willywonka.attlocal.net>

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,7 +37,7 @@ const config = {
                 use: ['babel-loader']
             },
             {
-                test: /\.(ico|svg)$/,
+                test: /\.(ico|svg|jpg)$/,
                 loader: 'file-loader'
             }
         ]
