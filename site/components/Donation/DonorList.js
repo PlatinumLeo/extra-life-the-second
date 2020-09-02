c235c639d9be9d050fd4169dbede7ef1b08e905a
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-09-02 11:28:06 -0400

    Feat/donation bar alpha (#22)
    * chore: bar graph 1
    
    * chore: bar graph 1
    
    * feat: seperate list into sub components
    
    * chore: fix skeleton key issue

---
+++ site/components/Donation/DonorList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+    Avatar,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Typography
+} from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+const mapStateToProps = state => ({
+    donors: state.donors
+});
+
+const DonorList = ({ donors }) => {
+    if (!donors || donors.length == 0) {
+        return (
+            <React.Fragment>
+                <Typography variant="h3">
+                    Biggest Donor
+                </Typography>
+                <List>
+                    {[1, 2, 3].map((item) => (
+                        <ListItem alignItems="flex-start" key={`donor-empty-${item}`}>
+                            <ListItemAvatar><Skeleton variant="circle" /></ListItemAvatar>
+                            <ListItemText><Skeleton /><Skeleton /></ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
+            </React.Fragment>
+        );
+    } else {
+        return (
+            <React.Fragment>
+                <Typography variant="h3">
+                    Biggest Donor
+                </Typography>
+                <List>
+                    {donors.map((item) => (
+                        <ListItem alignItems="flex-start" key={`item-${item.donorId}`}>
+                            <ListItemAvatar>
+                                <Avatar alt={item.displayName ? item.displayName : "Anonymous Donor"} src={item.avatarImageURL}/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={item.displayName ? item.displayName : "Anonymous Donor"}
+                                secondary={`$${item.sumDonations}`} />
+                        </ListItem>
+                    ))}
+                </List>
+            </React.Fragment>
+        );
+    }
+};
+
+export default connect(mapStateToProps)(DonorList);
--- site/components/Donation/index.js
+++ site/components/Donation/index.js
@@ -1,80 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import './Donation.css'
 
 import {
-    Avatar,
     GridList,
     GridListTile,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText,
     Typography
 } from '@material-ui/core';
-import Skeleton from '@material-ui/lab/Skeleton'
+import Skeleton from '@material-ui/lab/Skeleton';
 
-import { TeamActions } from '../../actions';
-import ExtraLifeService from '../../services/ExtraLifeService';
+import DonationList from './DonationList';
+import DonorList from './DonorList';
 
 const mapStateToProps = state => ({
-    donations: state.donations,
-    donors: state.donors,
     fundraisingGoal: state.team.fundraisingGoal,
     sumDonations: state.team.sumDonations
 });
 
-const Donation = ({ donations, donors, fundraisingGoal, sumDonations }) => {
+const Donation = ({ fundraisingGoal, sumDonations }) => {
     const goalComponent = () => {
         if (!fundraisingGoal || !sumDonations) {
             return (<Skeleton variant="text" />);
         }
 
-        return (
-            <Typography variant="h2">
-                Goal: ${sumDonations}/${fundraisingGoal}
-            </Typography>
-        );
-    };
+        let percent = Math.min(100, Math.max(1, Math.floor(sumDonations / fundraisingGoal * 100)));
 
-    const donationsComponent = () => {
-        if (!donations) {
-            return (<Skeleton variant="text" />);
-        }
+        const spanStyle = {
+            width: `${percent}%`
+        };
 
         return (
-            <List>
-                {donations.map((item) => (
-                    <ListItem alignItems="flex-start" key={`item-${item.donationID}`}>
-                        <ListItemAvatar>
-                            <Avatar alt={item.displayName ? item.displayName : "Anonymous Donor"} src={item.avatarImageURL}/>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={item.displayName ? item.displayName : "Anonymous Donor"}
-                            secondary={`$${item.amount}`} />
-                    </ListItem>
-                ))}
-            </List>
-        )
-    };
-
-    const donorComponent = () => {
-        if (!donors) {
-            return (<Skeleton variant="text" />);
-        }
-
-        return (
-            <List>
-                {donors.map((item) => (
-                    <ListItem alignItems="flex-start" key={`item-${item.donorId}`}>
-                        <ListItemAvatar>
-                            <Avatar alt={item.displayName ? item.displayName : "Anonymous Donor"} src={item.avatarImageURL}/>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={item.displayName ? item.displayName : "Anonymous Donor"}
-                            secondary={`$${item.sumDonations}`} />
-                    </ListItem>
-                ))}
-            </List>
+            <React.Fragment>
+                <Typography variant="h2">
+                    Goal: ${sumDonations}/${fundraisingGoal}
+                </Typography>
+                <div className="meter">
+                    <span style={spanStyle}><span></span></span>
+                </div>
+            </React.Fragment>
         );
     };
 
@@ -87,8 +50,8 @@ const Donation = ({ donations, donors, fundraisingGoal, sumDonations }) => {
                 <GridListTile>{goalComponent()}</GridListTile>
             </GridList>
             <GridList cols={2}>
-                <GridListTile>{donationsComponent()}</GridListTile>
-                <GridListTile>{donorComponent()}</GridListTile>
+                <GridListTile><DonationList /></GridListTile>
+                <GridListTile><DonorList /></GridListTile>
             </GridList>
         </React.Fragment>
     );
