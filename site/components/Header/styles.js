b17d3bd36b9e35a683ed64c3601fcf61e08072b9
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-09-07 11:40:08 -0400

    chore: add new items - Add Explaination Widget - Update Header Component

--- site/components/Header/styles.js
+++ site/components/Header/styles.js
@@ -1,26 +1,26 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    title: {
-      flexGrow: 1,
-      fontFamily: 'Russo One',
-      paddingLeft: 10,
-    },
-    image: {
-      height: 45,
-    },
-    buttonText: {
-      fontFamily: 'Russo One', 
-      paddingTop: 2, 
-      paddingRight: 10,
-    },
-    buttonImage: {
-      width: 25, 
-      height: 25
-    }
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: 'Russo One',
+    paddingLeft: 10,
+  },
+  image: {
+    height: 45,
+  },
+  buttonText: {
+    fontFamily: 'Russo One', 
+    paddingTop: 2, 
+    paddingRight: 10,
+  },
+  buttonImage: {
+    width: 25, 
+    height: 25
+  }
+}));
 
 export default useStyles;
--- site/pages/LandingPage/index.js
+++ site/pages/LandingPage/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
 import {
-    Grid,
-    Typography
+  Grid,
+  Typography
 } from '@material-ui/core';
 
 import Countdown from '../../components/Countdown';
 import Donation from '../../components/Donation';
+import Explaination from '../../components/Explaination';
 
 const LandingPage = props => {
-    const dayOfPlay = new Date("Nov 11, 2020 11:00:00").getTime();
+  const dayOfPlay = new Date("Nov 11, 2020 11:00:00").getTime();
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12} lg={6}>
-                <Donation />
-            </Grid>
-            <Grid item xs={12} lg={6}>
-                <Typography variant="h2">Day of Play: November 7th 11:00 AM ET</Typography>
-                <Countdown dayOfPlay={dayOfPlay} />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} lg={6}>
+        <Explaination />
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <Donation />
+      </Grid>
+      {/* <Grid item xs={12} lg={6}>
+        <Typography variant="h2">Day of Play: November 7th 11:00 AM ET</Typography>
+        <Countdown dayOfPlay={dayOfPlay} />
+      </Grid> */}
+    </Grid>
+  );
 };
 
 export default LandingPage;
