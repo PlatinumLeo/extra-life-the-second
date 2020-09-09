7fe9563073f6b20890baded0fb559eb7032441fd
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-09-09 10:53:36 -0400

    Feat/better ui (#27)
    * chore: add new items
    - Add Explaination Widget
    - Update Header Component
    
    * chore: add stream page

---
+++ site/components/Stream/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+
+const Stream = props => {
+  return (
+    <ReactTwitchEmbedVideo align="center" channel="pyroticblaziken" />
+  );
+};
+
+export default Stream;
--- site/pages/CommunityPage/index.js
+++ site/pages/CommunityPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+  Grid,
   Link,
   Typography,
   List,
@@ -8,14 +9,28 @@ import {
   ListItemText
 } from '@material-ui/core';
 
+import Stream from '../../components/Stream';
 import Twitch from '../../components/Twitch';
 
 const CommunityPage = props => {
   return (
-    <React.Fragment>
-      <Typography variant="h2">Community Page</Typography>
-      <Twitch />
-    </React.Fragment>
+    <Grid container>
+      <Grid item xs={12}>
+        <Stream />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Typography variant="h4" align="center">What is this?</Typography>
+        <Typography>
+          We want to test out the stream and make sure we have all of the processes worked out. Alex will also probably be streaming practice of his Mega Man X4 speed runs here.
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={8}>
+        <Typography variant="h4" align="center">Upcoming Events</Typography>
+        <Typography>
+          Leading up to the Day of Play on November 7th, 11 am EST, we may hold various events to get people hype! Here will be a list of upcoming events people can participate in.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
