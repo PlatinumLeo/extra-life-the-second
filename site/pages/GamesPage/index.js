a2f46626447bd5701faacb6c7b1db38d6863933a
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-09-20 10:49:03 -0400

    Feat/26/game list (#29)
    * chore: game list updates
    
    * chore: update games list

--- site/pages/GamesPage/index.js
+++ site/pages/GamesPage/index.js
@@ -22,7 +22,98 @@ const gameList = [
     links: ['https://mb.srb2.org/showthread.php?t=43708&page=3'],
     ageRating: 'E',
     equipment: 'Computer (Controller Recommended)'
-  }
+  },
+  {
+    name: 'Among Us',
+    description: 'Game of Deception and Murder',
+    links: ['http://www.innersloth.com/gameAmongUs.php'],
+    ageRating: 'E',
+    equipment: 'Computer, Mobile Device'
+  },
+  {
+    name: 'Fall Guys',
+    description: 'MMXC/Wipeout multiplayer game',
+    links: ['https://fallguys.com/'],
+    ageRating: 'E',
+    equipment: 'PS4, Computer (Controller Recommended)'
+  },
+  {
+    name: 'Rocket League',
+    description: 'Car based Soccer',
+    links: ['https://www.rocketleague.com/'],
+    ageRating: 'E',
+    equipment: 'PS4, XBox One, Switch, Computer (Controller Recommended)'
+  },
+  {
+    name: 'JackBox Games',
+    description: 'Fun Party Games',
+    links: ['https://www.jackboxgames.com/'],
+    ageRating: 'E',
+    equipment: 'Computer, Mobile Device'
+  },
+  {
+    name: 'Mario Kart 8 Deluxe (Swtich)',
+    description: 'Mario Kart',
+    links: ['https://mb.srb2.org/showthread.php?t=43708&page=3'],
+    ageRating: 'E',
+    equipment: 'Switch'
+  },
+  {
+    name: 'Puyo Puyo Tetris',
+    description: 'Play either Puyo Puyo, Tetris, or Both at once!',
+    links: ['https://puyo.sega.com/tetris/'],
+    ageRating: 'E',
+    equipment: 'PS4, Switch, Computer'
+  },
+  {
+    name: 'Skribblio',
+    description: 'Pictonary on the Web',
+    links: ['https://skribbl.io/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
+  {
+    name: 'Valorant',
+    description: '5v5 Tactical Shooter',
+    links: ['https://playvalorant.com/en-us/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
+  {
+    name: 'Spellbreak',
+    description: 'Free to play Mario Kart clone on PC',
+    links: ['https://playspellbreak.com/en'],
+    ageRating: 'E',
+    equipment: 'PS4, XBox One, Switch, Computer (Epic)'
+  },
+  {
+    name: 'One-Night Werewolf',
+    description: 'One Night Werewolf',
+    links: ['https://netgames.io/games/onu-werewolf/', 'https://werewolv.es/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
+  {
+    name: 'Mafia',
+    description: 'Mafia',
+    links: ['https://mafia.gg', 'https://epicmafia.com/lobby/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
+  {
+    name: 'Secret Hitler',
+    description: 'Politics and Intrigue Game',
+    links: ['https://private.secrethitler.io/', 'https://netgames.io/games/secret-hitler/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
+  {
+    name: 'Uno',
+    description: 'Uno',
+    links: ['http://play-uno.com/newgame.php', 'https://play.unofreak.com/'],
+    ageRating: 'E',
+    equipment: 'Computer'
+  },
 ];
 
 const GamesPage = props => {
