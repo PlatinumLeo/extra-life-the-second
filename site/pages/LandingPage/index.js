c3fc24eee6c0aff3caa677613bfce8dc3104e431
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-09-28 10:29:42 -0400

    chore: continued clean up work

--- site/pages/LandingPage/index.js
+++ site/pages/LandingPage/index.js
@@ -4,6 +4,7 @@ import {
   Grid,
   Typography
 } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
 
 import Countdown from '../../components/Countdown';
 import Donation from '../../components/Donation';
@@ -13,17 +14,25 @@ const LandingPage = props => {
   const dayOfPlay = new Date("Nov 11, 2020 11:00:00").getTime();
 
   return (
-    <Grid container spacing={3}>
+    <Grid container> {/*} spacing={3}>*/}
       <Grid item xs={12} lg={6}>
         <Explaination />
+        {/* <div style={{
+          backgroundColor: 'red',
+          height: '300px'
+        }}/> */}
+      </Grid>
+      <Grid container item xs={12} lg={6}>
+        <Donation />
+        {/* <div style={{
+          backgroundColor: 'blue',
+          height: '300px'
+        }}/> */}
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Donation />
-      </Grid>
-      {/* <Grid item xs={12} lg={6}>
         <Typography variant="h2">Day of Play: November 7th 11:00 AM ET</Typography>
         <Countdown dayOfPlay={dayOfPlay} />
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 };
