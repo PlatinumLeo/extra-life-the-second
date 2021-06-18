f3c90f924779218c49367b22d3731ae0c5abb41c
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-06-18 19:26:23 -0400

    chore: capture work for custom fonts

--- site/theme/monochrome.js
+++ site/theme/monochrome.js
@@ -1,11 +1,35 @@
 import { createMuiTheme } from '@material-ui/core';
 import { blueGrey, grey } from '@material-ui/core/colors';
 
+import QuanticoRegular from '../assets/fonts/Quantico/Quantico-Regular.ttf';
+
+const quantico = {
+  fontFamily: 'Quantico',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    local('Quantico'),
+    local(Quantico-Regular),
+    url(${QuanticoRegular}) format('truetype')
+  `// ,
+  // unicodeRange: ''
+};
+
 export const monochromeTheme = createMuiTheme({
   palette: {
     type: 'light',
     primary: grey,
     secondary: blueGrey
+  },
+  typography: {
+    fontFamily: 'Quantico, Arial, Roboto'
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [quantico]
+      }
+    }
   }
 });
-
