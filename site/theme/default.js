8ea72b992ae9408e947e677b82d72c43d5ebfbd8
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-06-22 13:13:33 -0400

    chore: default theme legible

--- site/theme/default.js
+++ site/theme/default.js
@@ -1,23 +1,30 @@
 import { createMuiTheme } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 
 export const defaultTheme = (type = 'light') => {
   return createMuiTheme({
     palette: {
       type,
       primary: {
+        light: '#307ba4',
         main: '#18527a',
+        dark: '#002a52'
       },
       secondary: {
+        light: '#c9f774',
         main: '#97c93c',
+        dark: '#77a326'
       },
       background: {
         default: '#fff',
         lightGrey: '#fcfcfc',
       },
-      text: {
-        primary: '#394141',
-        secondary: '#010111'
-      }
+      // text: {
+      //   primary: {
+      //     main: grey[800],
+      //   },
+      //   secondary: grey[900]
+      // }
     },
     typography: {
       fontFamily: 'Monserrat, Arial, Russo One'
