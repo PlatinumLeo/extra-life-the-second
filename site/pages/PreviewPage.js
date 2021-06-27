636e8c27281c107f65206a6bf14a62071bcc2889
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-06-27 19:50:58 -0400

    chore: working preview page

---
+++ site/pages/PreviewPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+const PreviewPage = props => {
+  return (
+    <div>
+      <Typography>Preview Page</Typography>
+    </div>
+  );
+};
+
+export default PreviewPage;
