569219a2232fa7424a46f3220024c210239ebbac
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-25 07:30:34 -0400

    chore: incorperate everyone's games

--- site/components/Team/teamData.js
+++ site/components/Team/teamData.js
@@ -7,6 +7,12 @@ import lilyLiImage from '../../assets/images/Profiles/lilyLi.png';
 import jordanLeahey from '../../assets/images/Profiles/jordanLeahey.jpeg';
 
 import sonic3Image from '../../assets/images/Games/sonic3andknuckles.png';
+import amplitudeImage from '../../assets/images/Games/amplitude.jpg';
+import tomAndJerryImage from '../../assets/images/Games/tomAndJerry.jpg';
+import zeldaNES from '../../assets/images/Games/tlozNES.png';
+import pokemonSilver from '../../assets/images/Games/pokemonSilver.jpg';
+import tetris from '../../assets/images/Games/tetris.jpg';
+import minishCap from '../../assets/images/Games/minishCap.jpg';
 
 export default [{
   name: 'Alex Lyons',
@@ -21,40 +27,40 @@ export default [{
   game: 'Amplitude',
   description: "At the ripe age of 3, I received a Game Boy Color from my parents, and my life was forever changed. Ever since then, I've been absolutely obsessed with all things video games. My favorite game from my childhood was Amplitude from Harmonix Studios. The precursor to Harmonix\'s big breakthrough game, Guitar Hero, Amplitude was a rhythm game where you controlled a space ship flying between the various tracks of a song in order to score points. Whenever a section of a track was played successfully, the music would auto-play for a period of time, allowing you to move onto the other instruments. By combo-ing these track loops, you could earn combo multipliers and really begin to play every part of the song. And with great tracks from artists such as David Bowie, P!nk, Slipknot, Weezer, and others, a music-obsessed kid like me could fall in love with all the beautiful sights, electric sounds, and addicting gameplay.",
   image: zachGuthrieImage,
-  gameImage: sonic3Image
+  gameImage: amplitudeImage
 }, {
   name: 'Shah Zafrani',
   discordTag: 'Anonymous#1337',
   game: 'Tom & Jerry (GBC)',
   description: "There was this Tom & Jerry platformer for the Gameboy Color. It controlled worse than Mario and the art wasn't nearly as good. What I loved about it was that the levels were set in locations like what you'd see in the cartoon: a kitchen, a garage, rooftops and rain gutters. I've gone through dozens of AA batteries on that game alone.",
   image: shahZafraniImage,
-  gameImage: sonic3Image
+  gameImage: tomAndJerryImage
 }, {
   name: 'Anthony Luaders',
   discordTag: 'Antman#4767',
   game: 'The Legend of Zelda (1986)',
   description: "On a sunny morning, many, many years ago, a young boy wiped off the dust of an original Atari. He put in Defender, Boxing, Ninja, and was hooked! From that day forward he was meant to be the lead in Player One, but Mr. Steven took too long, and the kid grew too old... The video game that truly got me hooked was The Legend of Zelda. I loved the way you could increase the health, pick up better items, solve puzzles, and the story! To this day, I can still hear the theme music, the fairy music, the sounds when Link took damage. All great memories.  ",
   image: anthonyLuadersImage,
-  gameImage: sonic3Image
+  gameImage: zeldaNES
 }, {
   name: 'Joshua Davis',
   discordTag: 'Anonymous#1337',
   game: 'Pokemon Silver',
   description: "My first memories of playing video games were with the Atari, a single joystick, console, and bulky TV that generated way too much heat. But, I didn’t get into gaming until I stole my older sister’s Gameboy Color and copy of Pokémon Silver. The cartridge was finicky and if you moved too hard or jostled it the wrong way, all saved data would be lost. Surprisingly, that didn’t stop me from going back again and again, starting over and testing how far I could get until the game was corrupted.",
   image: joshuaDavisImage,
-  gameImage: sonic3Image
+  gameImage: pokemonSilver
 }, {
   name: 'Lily Li',
   discordTag: 'Anonymous#1337',
   game: 'Tetris',
   description: "Games were pretty much banned when I was a kid, but my parents finally caved when I was 10 and stuck in a hospital for 2 weeks with nothing to do. They gave me the tiniest handheld Tetris game and I am still an addict to this day. When I'm not zoning out and imagining tetrominoes falling into place, you can find me on my Animal Crossing island.",
   image: lilyLiImage,
-  gameImage: sonic3Image
+  gameImage: tetris
 }, {
   name: 'Jordan Leahey',
   discordTag: 'Anonymous#1337',
   game: 'The Legend of Zelda: Minish Cap',
   description: "My earliest memory of videogames were when I got a Gameboy Color and  Pokemon Gold for Christmas when I was in pre-school or kindergarten. Thats really what started it all, and I went on to struggle trying to play many games on non-lit backlit gameboy screens. One that really sticks out to me is the Legend of Zelda: Minish Cap. The gimmick to this game is that Link could shrink so it added an extra layer to all of the areas of the game. It’s not a very well-known Zelda game, but I played through it so many times trying to find all of the secrets I could.",
   image: jordanLeahey,
-  gameImage: sonic3Image
+  gameImage: minishCap
 }];
