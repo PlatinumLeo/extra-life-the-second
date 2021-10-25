380ee39022e723d3536eb8de5b57b84a0634c1ca
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-25 08:06:52 -0400

    Feat/team info display (#77)
    * chore: work on modal
    
    * chore: incorperate everyone's games
    
    * chore: remove modal until fix found

--- site/components/Team/TeamTile.js
+++ site/components/Team/TeamTile.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Box,
   GridListTile,
-  GridListTileBar
+  GridListTileBar,
+  Modal,
+  Typography
 } from '@material-ui/core';
 
 import './teamTile.css';
 
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  background: '#fafafa',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
 const TeamTile = ({ data }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   let {
     name, discordTag, game, description, image, gameImage
   } = data;
 
   return (
-    <GridListTile style={{ height: '328px', width: '328px', margin: '16px' }}>
+    <GridListTile style={{ height: '328px', width: '328px', margin: '16px' }} onClick={handleOpen}>
       <div className="card">
         <div className="content">
           <div className="front">
@@ -26,6 +50,13 @@ const TeamTile = ({ data }) => {
           </div>
         </div>
       </div>
+      {/* <Modal
+        open={open}
+        onClose={handleClose}>
+          <Box style={modalStyle}>
+            <Typography>{description}</Typography>
+          </Box>
+      </Modal> */}
     </GridListTile>
   );
 };
