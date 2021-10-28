41aafdfb0b37798146312d930f415b090f68042f
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-28 08:31:11 -0400

    chore: spacing updates

--- site/components/Team/index.js
+++ site/components/Team/index.js
@@ -16,7 +16,7 @@ const Team = props => {
       <Typography variant="h3" align="center" style={{ textTransform: 'uppercase' }}>Meet the Team</Typography>
       <hr style={{ width: '100px', border: '3px solid' }} />
       <Typography align="center" style={{ maxWidth: '700px' }}>
-      Our Extra Life team is comprised of Slalom employees who all feel passionate about gaming and helping out those who need it most; children and their families who are in need of medical assistance.
+        Our Extra Life team is comprised of Slalom employees who all feel passionate about gaming and helping out those who need it most; children and their families who are in need of medical assistance.
       </Typography>
       <GridList cellHeight={360} cols={4} padding={20} style={{ maxWidth: '1440px' }}>
         {teamData.map((data, i) => (<TeamTile key={`team-tile-${i}`} data={data}/>))}
