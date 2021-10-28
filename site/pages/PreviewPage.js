031535bcfb1660535cd54bb36a25762011873127
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-28 08:45:49 -0400

    Feat/countdown upgrade (#78)
    * chore: remove unused components
    
    * chore: spacing updates
    
    * chore: make sponsor look right

--- site/pages/PreviewPage.js
+++ site/pages/PreviewPage.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
 
 import App from '../App.v2';
-
-import Header from '../components/Header.v2';
 
 const PreviewPage = props => {
   return (
