668ec09e387bb4ed1a104ec072691681cf736f4a
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-28 22:39:36 -0400

    chore: update schedule

--- site/components/Schedule/index.js
+++ site/components/Schedule/index.js
@@ -1,54 +1,49 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Paper } from '@material-ui/core';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
   Appointments,
-  Toolbar,
-  DateNavigator,
   CurrentTimeIndicator
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-const firstDay = '2020-11-07';
-const secondDay = '2020-11-08';
+import { schedule2021 as schedule } from './scheduleData';
 
-const mapStateToProps = state => ({
-    schedule: state.schedule
-});
+const firstDay = '2021-11-06';
+const secondDay = '2021-11-07';
 
-const Schedule = ({ schedule }) => {
+const Schedule = () => {
     return (
-        <React.Fragment>
-            <Paper>
-                <Scheduler data={schedule}>
-                    <ViewState currentDate={firstDay} />
-                    <DayView 
-                        startDayHour={11}
-                        endDayHour={24} />
-                    <Appointments />
-                    <CurrentTimeIndicator
-                        shadePreviousCells={true}
-                        shadePreviousAppointments={true}
-                        updateInterval={5 * 60 * 1000} />
-                </Scheduler>
-            </Paper>
-            <Paper>
-                <Scheduler data={schedule}>
-                    <ViewState currentDate={secondDay} />
-                    <DayView 
-                        startDayHour={0}
-                        endDayHour={12} />
-                    <Appointments />
-                    <CurrentTimeIndicator
-                        shadePreviousCells={true}
-                        shadePreviousAppointments={true}
-                        updateInterval={5 * 60 * 1000} />
-                </Scheduler>
-            </Paper>
-        </React.Fragment>
+      <React.Fragment>
+        <Paper>
+            <Scheduler data={schedule}>
+              <ViewState currentDate={firstDay} />
+              <DayView 
+                startDayHour={12}
+                endDayHour={24} />
+              <Appointments />
+              <CurrentTimeIndicator
+                shadePreviousCells={true}
+                shadePreviousAppointments={true}
+                updateInterval={5 * 60 * 1000} />
+            </Scheduler>
+        </Paper>
+        <Paper>
+            <Scheduler data={schedule}>
+              <ViewState currentDate={secondDay} />
+              <DayView 
+                startDayHour={0}
+                endDayHour={13} />
+              <Appointments />
+              <CurrentTimeIndicator
+                shadePreviousCells={true}
+                shadePreviousAppointments={true}
+                updateInterval={5 * 60 * 1000} />
+            </Scheduler>
+        </Paper>
+      </React.Fragment>
     );
 };
 
-export default connect(mapStateToProps)(Schedule);
+export default Schedule;
---
+++ site/components/Schedule/scheduleData.js
@@ -0,0 +1,83 @@
+export const schedule2021 = [{
+  startDate: '2021-11-06T12:00',
+  endDate: '2021-11-06T12:15',
+  title: "Opening Ceremonies"
+}, {
+  startDate: '2021-11-06T12:15',
+  endDate: '2021-11-06T14:00',
+  title: "Smash Bros. Casuals"
+}, {
+  startDate: '2021-11-06T14:15',
+  endDate: '2021-11-06T16:15',
+  title: "JackBox Games"
+}, {
+  startDate: '2021-11-06T16:30',
+  endDate: '2021-11-06T18:00',
+  title: "Sonic Robo Blast 2 Kart"
+}, {
+  startDate: '2021-11-06T18:15',
+  endDate: '2021-11-06T19:30',
+  title: "Mega Man X4 Speed Run"
+}, {
+  startDate: '2021-11-06T19:45',
+  endDate: '2021-11-06T21:15',
+  title: "Super Mario Party"
+}, {
+  startDate: '2021-11-06T21:30',
+  endDate: '2021-11-07T00:00',
+  title: "Random Bullshit Hours"
+}, {
+  startDate: '2021-11-07T00:00',
+  endDate: '2021-11-07T06:00',
+  title: "Devil May Cry V"
+}, {
+  startDate: '2021-11-07T06:15',
+  endDate: '2021-11-07T11:40',
+  title: "More Random Bullshit"
+}, {
+  startDate: '2021-11-07T11:45',
+  endDate: '2021-11-07T12:00',
+  title: "Closing Ceremonies"
+}];
+
+export const schedule2020 = [{
+  startDate: '2020-11-07T11:00',
+  endDate: '2020-11-07T11:15',
+  title: "Opening Ceremonies"
+}, {
+  startDate: '2020-11-07T11:30',
+  endDate: '2020-11-07T13:00',
+  title: "Smash Bros. Casuals"
+}, {
+  startDate: '2020-11-07T13:30',
+  endDate: '2020-11-07T16:30',
+  title: "Rocket League"
+}, {
+  startDate: '2020-11-07T17:00',
+  endDate: '2020-11-07T19:00',
+  title: "Random Nonsense!"
+}, {
+  startDate: '2020-11-07T19:15',
+  endDate: '2020-11-07T19:45',
+  title: "Raffle Winner Announcements!"
+}, {
+  startDate: '2020-11-07T20:00',
+  endDate: '2020-11-07T21:30',
+  title: "Mega Man X4 Speed Run"
+}, {
+  startDate: '2020-11-07T22:00',
+  endDate: '2020-11-08T00:00',
+  title: "Among Us"
+}, {
+  startDate: '2020-11-08T00:00',
+  endDate: '2020-11-08T06:00',
+  title: "Devil May Cry V"
+}, {
+  startDate: '2020-11-08T09:00',
+  endDate: '2020-11-08T11:00',
+  title: "Kartastrophy (SRB2K)"
+}, {
+  startDate: '2020-11-08T11:00',
+  endDate: '2020-11-08T11:15',
+  title: "Closing Ceremonies"
+}];
