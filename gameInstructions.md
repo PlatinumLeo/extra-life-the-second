26331e36bf9c4fd9a4ee13b451933177e1593f71
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-29 16:09:09 -0400

    Break/transition to v2 (#83)
    * chore: new website
    
    * chore: update schedule
    
    * chore: capture game instructions
    
    * breaking: update packages
    
    * breaking: remove old packages finish
    
    * chore: final fix to packages

---
+++ gameInstructions.md
@@ -0,0 +1,18 @@
+# Game Instructions
+
+## Sonic Robo Blast 2 Kart
+
+1. Download Sonic Robo Blast 2 Kart (SRB2K) [here](https://github.com/STJr/Kart-Public/releases).
+2. Place the game files somewhere you will remember.
+3. Be sure to connect a controller before starting the game.
+4. To Configure your controller (tested with a Xbox One Controller on PC)
+
+Take Screen Shots to describe how to set up controller
+
+Take Screen Shots to describe how to select your character
+
+Have links to tutorials for those who want them
+
+Have video link to ScottFalco's Side of Salt about SRB2K
+
+Add to server with link and all of that jazz
--- site/index.js
+++ site/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { compose, createStore } from 'redux';
 
 import './index.css';
 import App from './App.v2';
 import ThemeProvider from './ThemeProvider';
 import TeamProvider from './TeamProvider';
 import DonationsProvider from './DonationsProvider';
-import allMiddleware from './middleware';
-import rootReducer from './reducers';
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    rootReducer,
-    composeEnhancer(allMiddleware)
-);
 
 render(
       <ThemeProvider>
