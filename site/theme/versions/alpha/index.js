336e28cfea9b2e771076754c7556912f015e28a9
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-30 08:19:26 -0400

    chore: fix createTheme

--- site/theme/versions/alpha/index.js
+++ site/theme/versions/alpha/index.js
@@ -1,10 +1,10 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
 import { typography } from './typography';
 import { overrides } from './overrides';
 import { palette } from './palette';
 
 export const alphaTheme = (type = 'light') => {
-  return createMuiTheme({
+  return createTheme({
     palette: Object.assign({}, palette, { type }),
     typography,
     overrides,
--- site/theme/versions/monochrome/index.js
+++ site/theme/versions/monochrome/index.js
@@ -1,10 +1,10 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
 import { typography } from '../alpha/typography';
 import { overrides } from '../alpha/overrides';
 import { palette } from './palette';
 
 export const monochromeTheme = (type = 'light') => {
-  return createMuiTheme({
+  return createTheme({
     palette: Object.assign({}, palette, { type }),
     typography,
     overrides,
