5225b8849a8b61dbba41e6dccb024f6090847f08
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-30 22:21:20 -0400

    chore: DataDisplay Dynamic Sizing

--- site/components/DataDisplay.js
+++ site/components/DataDisplay.js
@@ -39,9 +39,9 @@ const getDataDisplayStyles = makeStyles((theme) => ({
   }
 }));
 
-const DataDisplayItem = ({ title, subtitle, className }) => {
+const DataDisplayItem = ({ title, subtitle, className, gridSize }) => {
   return (
-    <Grid item xs={4}>
+    <Grid item xs={gridSize}>
       <div className={className}>
         <Typography color='textSecondary' variant="h5">{title}</Typography>
         <Typography color='textSecondary' variant="caption" style={{ textTransform: 'uppercase' }}>{subtitle}</Typography>
@@ -53,12 +53,13 @@ const DataDisplayItem = ({ title, subtitle, className }) => {
 const DataDisplay = ({ className, dataPoints }) => {
   const theme = useTheme();
   const classes = getDataDisplayStyles(theme);
+  let gridSize = Math.floor(12 / dataPoints.length);
 
   return (
     <div className={`${className} ${classes.root}`}>
       <Grid container>
         {dataPoints.map(({ title, subtitle }, i) => (
-          <DataDisplayItem key={`display-${title}-${subtitle}`} title={title} subtitle={subtitle} className={(i < dataPoints.length - 1) ? classes.child : classes.lastChild} />
+          <DataDisplayItem key={`display-${title}-${subtitle}`} title={title} subtitle={subtitle} gridSize={gridSize} className={(i < dataPoints.length - 1) ? classes.child : classes.lastChild} />
         ))}
       </Grid>
     </div>
