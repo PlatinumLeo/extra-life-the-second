4ed8f6aa793e789aa139118f1dcbf7f7a1a86a8c
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-10-30 21:04:12 -0400

    chore: Sponsor Moblie Ready

--- site/components/Sponsor.js
+++ site/components/Sponsor.js
@@ -11,8 +11,10 @@ import TwitchLogo from './svg/TwitchLogo';
 
 const getSponsorStyles = makeStyles((theme) => ({
   root: { padding: '48px 0 48px 0', borderBottom: `1px solid ${theme.palette.common.black}` },
-  wrapper: { float: 'left', left: '50%', position: 'relative', padding: '0 24px 12px 24px' },
-  content: { float: 'left', left: '-50%', position: 'relative' }
+  wrapper: { padding: '0 24px 12px 24px' },
+  choa: { left: 'calc(50% - 99px)', position: 'relative' },
+  cmnh: { left: 'calc(50% - 70px)', position: 'relative' },
+  twitch: { left: 'calc(50% - 51px)', position: 'relative' },
 }));
 
 const Sponsor = props => {
@@ -21,9 +23,9 @@ const Sponsor = props => {
 
   return (
     <Grid container className={classes.root}>
-      <Grid item xs={12} className={classes.wrapper}><CHOALogo className={classes.content} /></Grid>
-      <Grid item xs={12} className={classes.wrapper}><CMNHLogo className={classes.content} /></Grid>
-      <Grid item xs={12} className={classes.wrapper}><TwitchLogo className={classes.content} /></Grid>
+      <Grid item xs={12} className={classes.wrapper}><CHOALogo className={classes.choa} /></Grid>
+      <Grid item xs={12} className={classes.wrapper}><CMNHLogo className={classes.cmnh} /></Grid>
+      <Grid item xs={12} className={classes.wrapper}><TwitchLogo className={classes.twitch} /></Grid>
     </Grid>
   )
 };
