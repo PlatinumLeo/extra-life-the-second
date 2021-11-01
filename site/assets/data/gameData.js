08ec517fcbb044e329955d75572717ccd170d5a0
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-11-01 14:13:45 -0400

    chore: begin work on games page

---
+++ site/assets/data/gameData.js
@@ -0,0 +1,7 @@
+
+
+export default [{
+  name: 'Sonic Robo Blast 2 Kart',
+  platforms: ['PC', 'Mac', 'Linux'],
+  
+}];
---
+++ site/components/Games/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core';
+
+const getStyles = makeStyles((theme) => ({
+  root: {
+    
+  }
+}));
+
+export default getStyles;
