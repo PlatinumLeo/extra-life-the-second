a3af7a8c81cb157b02c77f2fee08a13c9218de8c
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-11-02 10:40:52 -0400

    chore: remove team auto refresh

--- server/api/teams.js
+++ server/api/teams.js
@@ -1,36 +1,6 @@
 import axios from 'axios';
 import express from 'express';
 
-const TEAM_IDS = [
-  57288, // Slalom Atlanta
-];
-
-const FETCH_INTERVAL = 5 * 1000; // 5 Seconds will set to something higher for prod...
-
-/* Begin Interval*/
-const donors = [];
-const donations = [];
-
-setInterval(() => {
-  axios.get(`https://www.extra-life.org/api/teams/${teamId}/donations`)
-    .then(response => {
-      let newDonations = response.data;
-
-      if (donations.length == 0) {
-        donations.push.apply(donations, newDonations); // Just copy the new array, don't worry about 
-      } else {
-        newDonations.forEach((n) => {
-          if (!donations.find(d => d.donationId === n.donationId)) {
-            console.log(`We got a new donation!`);
-            donations.push(n);
-            // Broadcast logic here
-          }
-        });
-      }
-    }).catch(err => { console.error(err); });
-}, FETCH_INTERVAL);
-/* End Interval */
-
 const router = express.Router();
 
 router.get('/:teamId/donors', (req, res) => {
