f39e66c3620756cbe273781565c22e28ce783d36
Alexander Lyons <alexander.n.lyons@gmail.com>
2022-04-10 08:43:26 -0400

    fix: update Hero

---
+++ site/components/Hero/ThankYou.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Box,
+  Typography
+} from '@mui/material';
+
+const NEXT_STREAM_SX = {
+  letterSpacing: '0.08em',
+  padding: '0 24px 20px 24px',
+  textTransform: 'uppercase'
+};
+
+const THANK_YOU_SX = {
+  padding: {
+    mobile: '0 24px 20px 24px',
+    tablet: '0 96px 16px 96px',
+    desktop: '0 560px 32px 560px',
+  }
+};
+
+const ThankYou = ({ ...props }) => (
+  <Box>
+    <Typography align='center' variant={(breakpoint === 'desktop') ? 'h4' : 'h6' } sx={NEXT_STREAM_SX}>Thank you all so much!</Typography>
+    <Box sx={THANK_YOU_SX}>
+      <Typography align='center' display='inline'>We had a wonderful time streaming 25 hours and had a blast with everyone showing up! </Typography>
+      <Typography align='center' display='inline'>While the stream has completed, we are still raising money for the Children's Miracle Network. </Typography>
+      <Typography align='center' display='inline'>We hope to see everyone next year and even more people show up to have fun and help make the world a better place!</Typography>
+    </Box>
+  </Box>
+);
+
+export default ThankYou;
--- site/components/Hero/index.js
+++ site/components/Hero/index.js
@@ -1,22 +1,21 @@
 import React, { useContext } from 'react';
 import {
   Box,
-  Hidden,
-  Link,
   Typography
 } from '@mui/material';
 
-import Countdown from '../Countdown';
 import HeartProgressBar from '../HeartProgressBar';
 import { TeamContext } from '../../providers/DonorDriveApiProvider';
 import { BreakpointContext } from '../../providers/AdaptivityProvider';
-import getStyles from './styles';
 import backgroundImage from '../../assets/images/BackgroundImages/mmx4_still.jpg';
 import backgroundWebM from '../../assets/videos/megaManX4.webm';
 import backgroundMP4 from '../../assets/videos/megaManX4.mp4';
 import Stream from '../Stream';
 import { StreamContext } from '../../providers/StreamProvider';
 import { pastDayOfPlay } from '../../utils';
+
+import NextStream from './NextStream';
+import ThankYou from './ThankYou';
 
 const PROGRESS_BAR_SX = {
   padding: {
@@ -51,69 +50,62 @@ const CALLOUT_SX = {
   textTransform: 'uppercase',
 };
 
-const NEXT_STREAM_SX = {
-  letterSpacing: '0.08em',
-  padding: '0 24px 20px 24px',
-  textTransform: 'uppercase'
-};
-
-const THANK_YOU_SX = {
-  padding: {
-    mobile: '0 24px 20px 24px',
-    tablet: '0 96px 16px 96px',
-    desktop: '0 560px 32px 560px',
-  }
-};
-
-const COUNTDOWN_SX = {
-  margin: {
-    mobile: '0 24px 64px 24px',
-    tablet: '0px 48px 32px 48px',
-    desktop: '0px 512px 80px 512px',
-  }
-};
-
 const DONATION_SUM_CONTAINER_SX = { float: 'left', left: '50%', position: 'relative' };
 const DONATION_SUM_CONTENT_SX = { float: 'left', left: '-50%', position: 'relative' };
 
+const VIDEO_BACKGROUND_SX = {
+  position: 'absolute',
+  overflow: 'hidden',
+  zIndex: -2,
+  height: '100%',
+  width: '100%',
+  backgroundColor: (theme) => theme.palette.primary.main,
+  backgroundImage: `linear-gradient(76deg, #9EEEE1aa, #462DEAaa), url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  top: 0, left: 0,
+  '& video': {
+    position: 'absolute',
+    width: '100%',
+    display: 'block',
+    margin: 'auto',
+    top: '0px'
+  },
+  display: {
+    mobile: 'none',
+    laptop: 'block'
+  }
+};
+
+const VIDEO_OVERLAY_SX = {
+  background: `linear-gradient(76deg, #9EEEE1aa, #462DEAaa)`,
+  opacity: 1,
+  position: 'absolute',
+  overflow: 'hidden',
+  zIndex: -1,
+  height: '100%',
+  width: '100%',
+  top: 0, left: 0,
+  backdropFilter: 'blur(4px)'
+};
+
 const Hero = ({ dayOfPlay }) => {
-  const classes = getStyles();
   const { fundraisingGoal, sumDonations } = useContext(TeamContext);
   // const streams = useContext(StreamContext);
   const breakpoint = useContext(BreakpointContext);
 
-  const nextStream = () => (
-    <Box>
-      <Typography align='center' color='inherit' variant={(breakpoint === 'desktop') ? 'h4' : 'h6' } sx={NEXT_STREAM_SX}>Next stream starts in</Typography>
-      <Countdown dayOfPlay={dayOfPlay} sx={COUNTDOWN_SX} />
-    </Box>
-  );
-
-  const thankYou = () => (
-    <Box>
-      <Typography align='center' variant={(breakpoint === 'desktop') ? 'h4' : 'h6' } sx={NEXT_STREAM_SX}>Thank you all so much!</Typography>
-      <Box sx={THANK_YOU_SX}>
-        <Typography align='center' display='inline'>We had a wonderful time streaming 25 hours and had a blast with everyone showing up! </Typography>
-        <Typography align='center' display='inline'>While the stream has completed, we are still raising money for the Children's Miracle Network. </Typography>
-        <Typography align='center' display='inline'>We hope to see everyone next year and even more people show up to have fun and help make the world a better place!</Typography>
-      </Box>
-    </Box>
-  );
-
   const defaultHero = () => (
     <Box sx={ROOT_SX}>
-      <Box className={classes.videoBackground}>
-        <Hidden lgDown>
-          <video autoPlay loop muted playsInline>
-            <source src={backgroundWebM} type="video/webm" />
-            <source src={backgroundMP4} type="video/mp4" />
-          </video>
-        </Hidden>
+      <Box sx={VIDEO_BACKGROUND_SX}>
+        <video autoPlay loop muted playsInline>
+          <source src={backgroundWebM} type="video/webm" />
+          <source src={backgroundMP4} type="video/mp4" />
+        </video>
       </Box>
-      <Box className={classes.videoOverlay} />
+      <Box sx={VIDEO_OVERLAY_SX} />
       <Box sx={{ opacity: 1, zIndex: 4 }}>
         <Typography align='center' color='inherit' variant={(breakpoint === 'desktop') ? 'h1' : 'h3' } sx={CALLOUT_SX}>We play games to help children’s hospitals in the United States and Canada</Typography>
-        {pastDayOfPlay ? thankYou() : nextStream()}
+        {pastDayOfPlay ? <ThankYou /> : <NextStream props={{ breakpoint, dayOfPlay }} />}
         <HeartProgressBar sumDonations={sumDonations} fundraisingGoal={fundraisingGoal} sx={PROGRESS_BAR_SX} />
         <Box sx={DONATION_SUM_CONTAINER_SX}>
           <Box sx={DONATION_SUM_CONTENT_SX}>
