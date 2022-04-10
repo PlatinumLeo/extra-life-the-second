aac08494e3d50ee5cc1c0390a4cc5abec68e47a8
Alexander Lyons <alexander.n.lyons@gmail.com>
2022-04-10 15:24:48 -0400

    fix: update hot reload

--- server/development/hotReload.js
+++ server/development/hotReload.js
@@ -1,20 +1,17 @@
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
+export const applyWebpackDevHotMiddleware = (app) => {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
 
-import createConfig from '../../webpack.site.config';
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-export function applyWebpackHotDevMiddleware(app) {
+  const createConfig = require('../../webpack.site.config');
+  const NODE_ENV = process.env.NODE_ENV || 'development';
   const webpackConfig = createConfig(NODE_ENV, 'development');
   const compiler = webpack(webpackConfig);
 
   app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
+    publicPath: webpackConfig.output.publicPath
   }));
-
-  app.use(webpackHotMiddleware(compiler, { }));
+  app.use(webpackHotMiddleware(compiler));
 
   if (module.hot) {
     module.hot.accept();
